948d90bd48b699259af4be0bf85eae5c4b5e00c4
zloopo <seliayeu@ualberta.ca>
2021-02-11 13:21:03 -0700

    add new endpoints

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
-
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+app.get('/version', (req, res) => {
+  res.send('1')
+})
 app.listen(PORT, () => {
   console.log('server started on port 5000') // eslint-disable-line no-console
 })
